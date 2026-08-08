45e3a8a9bc0513c19997c433b7925e0c2b546745
Hameer Siddique <37618396+hameersiddique@users.noreply.github.com>
2026-08-08 19:43:29 +0300

    missing imports added

--- backend/src/modules/transactions/transactions.module.ts
+++ backend/src/modules/transactions/transactions.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Transaction, Category } from '../../entities';
+import { Transaction, Category, Subcategory, PaymentMethod, Account } from '../../entities';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { ExportService } from '../../common/services/export.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction, Category])],
+  imports: [TypeOrmModule.forFeature([Transaction, Category, Subcategory, PaymentMethod, Account])],
   controllers: [TransactionsController],
   providers: [TransactionsService, ExportService],
   exports: [TransactionsService],
